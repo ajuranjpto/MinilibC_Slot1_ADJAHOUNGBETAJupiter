4cbeee08afff281562a5ab9d938f2c8afc3ce390
Ajuranjpto <jupiter.adjahoungbeta@epitech.eu>
2024-03-01 10:38:47 +0100

    push fac.asm file

---
+++ fac.asm
@@ -0,0 +1,25 @@
+    section text
+    global my_add
+
+my_add:
+    xor rcx, rcx
+    cmp rsi, 0
+    je rsi_null
+    mov al, BYTE[rsi + rcx]
+    jmp loop
+
+loop:
+    cmp BYTE[rdi + rcx], al
+    je done
+    inc rcx
+    jmp loop
+
+done:
+    inc rcx
+    mov al, BYTE[rdi + rcx]
+    add al, 1
+    movsx rax, al
+    ret
+
+rsi_null:
+    ret
